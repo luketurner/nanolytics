3a411b3de44950eb01228591ef7cce577b678034
Luke Turner <github@luketurner.org>
2025-08-16 14:33:54 -0700

    ensure name/hostname cannot be empty

--- src/sites/schema.ts
+++ src/sites/schema.ts
@@ -3,12 +3,12 @@ import z from "zod/v4";
 
 export const siteSchema = z.object({
   id: z.uuid(),
-  name: z.string().max(32),
-  hostnames: z.preprocess(fromJSONString, z.array(z.string())),
+  name: z.string().min(1).max(32),
+  hostnames: z.preprocess(fromJSONString, z.array(z.string().min(1))),
 });
 
 export const siteSchemaClient = siteSchema.extend({
-  hostnames: z.array(z.string()),
+  hostnames: z.array(z.string().min(1)),
 });
 
 export type Site = z.infer<typeof siteSchema>;
